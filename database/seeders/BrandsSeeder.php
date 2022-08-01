8636d17723bcbe0479725e1b2b4a1e2fdd8af6a5
Joe Reynolds <joereynolds952@gmail.com>
2022-08-01 20:04:26 +0100

    Seed brands

---
+++ database/seeders/BrandsSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BrandsSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('brands')->insert(
+            [
+                ['name' => 'Alien Workshop'],
+                ['name' => 'Almost'],
+                ['name' => 'Baker'],
+                ['name' => 'Chocolate'],
+                ['name' => 'Darkstar'],
+                ['name' => 'Enjoi'],
+                ['name' => 'Element'],
+                ['name' => 'Flip'],
+                ['name' => 'Fracture'],
+                ['name' => 'Girl'],
+                ['name' => 'Krooked'],
+                ['name' => 'Santa Cruz'],
+                ['name' => 'Toy Machine'],
+            ],
+        );
+    }
+}
--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@
 ./artisan.sh key:generate
 ./artisan.sh config:clear
 ./artisan.sh config:cache
+./artisan.sh migrate:fresh --seed
